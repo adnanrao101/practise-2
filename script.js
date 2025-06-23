bba3d68bcd5e5cc6133fab1783c341ba7d109d8b
Adnan Rao <adnanejaz2874@gmail.com>
2025-06-23 16:44:36 +0500

    Increment / Decrement by js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let count = 0;
 
-const hoo = document.getElementById("H1");
-const hoo2 = document.getElementById("H2");
+const display = document.getElementById("H1");
+
 
 function h1change(){
     count++
-    hoo.innerHTML= count; 
+    display.innerHTML= count; 
 }
 function h2change(){
     count--
-    hoo2.innerHTML= count;
+    display.innerHTML= count;
 }
 
